471b2c8f8d7fd4426bf1164fa87ac2d439936dfb
gabriel <gabriel.nicola88@gmail.com>
2022-11-26 11:58:15 +0200

    test

--- compress.sh
+++ compress.sh
@@ -1,7 +1,6 @@
 mkdir -p src/declarations/test
-$(dfx cache show)/moc --idl src/test/Main.mo  --package base $(dfx cache show)/base -o src/declarations/test/test.did
-didc bind src/declarations/test/test.did -t js > src/declarations/test/test.did.js
-didc bind src/declarations/test/test.did -t ts > src/declarations/test/test.t.ts
-$(dfx cache show)/moc src/test/Main.mo  -o src/declarations/test/test.did  --package base $(dfx cache show)/base -o src/declarations/test/test.wasm
-
+$(dfx cache show)/moc src/test/Main.mo -c --debug --idl --stable-types --public-metadata candid:service --actor-idl src/declarations/test/idl/ --package base $(dfx cache show)/base -o src/declarations/test/test.wasm
+# didc bind src/declarations/test/test.did -t js > src/declarations/test/test.did.js
+# didc bind src/declarations/test/test.did -t ts > src/declarations/test/test.t.ts
+ic-wasm src/declarations/test/test.wasm -o src/declarations/test/test.wasm shrink
 gzip -c src/declarations/test/test.wasm > src/declarations/test/test.wasm.gz
